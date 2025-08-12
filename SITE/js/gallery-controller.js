513de3f9eae2efac81b344e6abff9a982e0faa2b
shanybaruch <baruchshany@gmail.com>
2025-08-12 14:47:08 +0300

    Create function renderGallery and gallery controller

--- SITE/js/gallery-controller.js
+++ SITE/js/gallery-controller.js
@@ -1,5 +1,23 @@
 'use strict'
 
+gImgs = [
+    {src: `SITE/img/square/1.jpg`},
+    {src: `SITE/img/square/2.jpg`},
+    {src: `SITE/img/square/3.jpg`},
+    {src: `SITE/img/square/4.jpg`},
+    {src: `SITE/img/square/5.jpg`},
+    {src: `SITE/img/square/6.jpg`},
+    {src: `SITE/img/square/7.jpg`},
+    {src: `SITE/img/square/8.jpg`},
+    {src: `SITE/img/square/9.jpg`},
+    {src: `SITE/img/square/10.jpg`},
+]
+
 function renderGallery() {
-    
+    var elGallery = document.querySelector('.imgs')
+    var strHtml = ''
+    for (var i = 0; i < gImgs.length; i++) {
+        strHtml += `<img src="${gImgs[i].src}"></img>`
+    }    
+    elGallery.innerHTML += strHtml
 }
