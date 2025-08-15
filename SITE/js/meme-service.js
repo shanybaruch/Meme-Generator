3751368bc4dd2f0cce9fb5a4a2dc71e8b5599665
shanybaruch <baruchshany@gmail.com>
2025-08-15 20:06:25 +0300

    Test

--- SITE/js/meme-service.js
+++ SITE/js/meme-service.js
@@ -5,16 +5,16 @@ var MEME_KEY = 'memeDB'
 var gMemeColor = '#91f6fd'
 var gImgNum = 3
 var gImgs = [
-    { id: 1, url: '../../SITE/img/square/1.jpg', keywords: ['funny', 'cat'] },
-    { id: 2, url: '../../SITE/img/square/2.jpg', keywords: ['funny', 'cat'] },
-    { id: 3, url: '../../SITE/img/square/3.jpg', keywords: ['funny', 'cat'] },
-    { id: 4, url: '../../SITE/img/square/4.jpg', keywords: ['funny', 'cat'] },
-    { id: 5, url: '../../SITE/img/square/5.jpg', keywords: ['funny', 'cat'] },
-    { id: 6, url: '../../SITE/img/square/6.jpg', keywords: ['funny', 'cat'] },
-    { id: 7, url: '../../SITE/img/square/7.jpg', keywords: ['funny', 'cat'] },
-    { id: 8, url: '../../SITE/img/square/8.jpg', keywords: ['funny', 'cat'] },
-    { id: 9, url: '../../SITE/img/square/9.jpg', keywords: ['funny', 'cat'] },
-    { id: 10, url: '../../SITE/img/square/10.jpg', keywords: ['funny', 'cat'] },
+    { id: 1, url: './SITE/img/square/1.jpg', keywords: ['funny', 'cat'] },
+    { id: 2, url: './SITE/img/square/2.jpg', keywords: ['funny', 'cat'] },
+    { id: 3, url: './SITE/img/square/3.jpg', keywords: ['funny', 'cat'] },
+    { id: 4, url: './SITE/img/square/4.jpg', keywords: ['funny', 'cat'] },
+    { id: 5, url: './SITE/img/square/5.jpg', keywords: ['funny', 'cat'] },
+    { id: 6, url: './SITE/img/square/6.jpg', keywords: ['funny', 'cat'] },
+    { id: 7, url: './SITE/img/square/7.jpg', keywords: ['funny', 'cat'] },
+    { id: 8, url: './SITE/img/square/8.jpg', keywords: ['funny', 'cat'] },
+    { id: 9, url: './SITE/img/square/9.jpg', keywords: ['funny', 'cat'] },
+    { id: 10, url: './SITE/img/square/10.jpg', keywords: ['funny', 'cat'] },
 ]
 var gMeme = {
     selectedImgId: `SITE/img/square/${gImgNum}.jpg`,
