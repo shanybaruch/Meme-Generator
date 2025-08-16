29f5c2b57ee6509d745991b1ede887ab8ef023d8
shanybaruch <baruchshany@gmail.com>
2025-08-16 21:05:46 +0300

    Test

--- SITE/js/meme-service.js
+++ SITE/js/meme-service.js
@@ -4,17 +4,18 @@
 var MEME_KEY = 'memeDB'
 var gMemeColor = '#91f6fd'
 var gImgNum = 3
+var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
 var gImgs = [
-    { id: 1, url: './SITE/img/square/1.jpg', keywords: ['funny', 'cat'] },
-    { id: 2, url: './SITE/img/square/2.jpg', keywords: ['funny', 'cat'] },
-    { id: 3, url: './SITE/img/square/3.jpg', keywords: ['funny', 'cat'] },
-    { id: 4, url: './SITE/img/square/4.jpg', keywords: ['funny', 'cat'] },
-    { id: 5, url: './SITE/img/square/5.jpg', keywords: ['funny', 'cat'] },
-    { id: 6, url: './SITE/img/square/6.jpg', keywords: ['funny', 'cat'] },
-    { id: 7, url: './SITE/img/square/7.jpg', keywords: ['funny', 'cat'] },
-    { id: 8, url: './SITE/img/square/8.jpg', keywords: ['funny', 'cat'] },
-    { id: 9, url: './SITE/img/square/9.jpg', keywords: ['funny', 'cat'] },
-    { id: 10, url: './SITE/img/square/10.jpg', keywords: ['funny', 'cat'] },
+    { id: 1, url: 'SITE/img/square/1.jpg', keywords: ['funny', 'cat'] },
+    { id: 2, url: 'SITE/img/square/2.jpg', keywords: ['funny', 'cat'] },
+    { id: 3, url: 'SITE/img/square/3.jpg', keywords: ['funny', 'cat'] },
+    { id: 4, url: 'SITE/img/square/4.jpg', keywords: ['funny', 'cat'] },
+    { id: 5, url: 'SITE/img/square/5.jpg', keywords: ['funny', 'cat'] },
+    { id: 6, url: 'SITE/img/square/6.jpg', keywords: ['funny', 'cat'] },
+    { id: 7, url: 'SITE/img/square/7.jpg', keywords: ['funny', 'cat'] },
+    { id: 8, url: 'SITE/img/square/8.jpg', keywords: ['funny', 'cat'] },
+    { id: 9, url: 'SITE/img/square/9.jpg', keywords: ['funny', 'cat'] },
+    { id: 10, url: 'SITE/img/square/10.jpg', keywords: ['funny', 'cat'] },
 ]
 var gMeme = {
     selectedImgId: `SITE/img/square/${gImgNum}.jpg`,
@@ -33,7 +34,6 @@ var gMeme = {
         }
     ]
 }
-var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
 
 
 function getMeme() {
